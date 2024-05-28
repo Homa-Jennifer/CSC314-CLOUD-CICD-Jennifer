c3b491a8a7429df6729fe89e2e1b79169966484c
christabelKC <136829472+christabelKC@users.noreply.github.com>
2024-05-28 17:34:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
 
 // app.use(express.static("public"));
 app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WIT AWS. THIS PAGE INDICATES SUCCESS. NAME: UGBOMOR CHRISTBEL KELECHI; MATRIC NUMBER: VUG/CSC/21/5275'));
