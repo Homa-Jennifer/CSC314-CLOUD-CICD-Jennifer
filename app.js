b348c4d7c3f5d7e9947a295b01be9a1ebf6e46bc
christbelKC <ugbomorchristabel@gmail.com>
2024-05-28 15:28:08 +0100

    main

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE !'));
+app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WIT AWS. THIS PAGE INDICATES SUCCESS. NAME: UGBOMOR CHRISTBEL KELECHI; MATRIC NUMBER: VUG/CSC/21/5275'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
