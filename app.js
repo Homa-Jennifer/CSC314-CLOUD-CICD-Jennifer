93f2d60118c59ce32b8fdd3c8df49f83a30adfee
Homa-Jennifer <140495820+Homa-Jennifer@users.noreply.github.com>
2024-05-29 14:08:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.port || 8080;
 
 app.use(express.static("public"));
-//app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WIT AWS. THIS PAGE INDICATES SUCCESS. NAME: UGBOMOR CHRISTBEL KELECHI; MATRIC NUMBER: VUG/CSC/21/5275'));
+//app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WIT AWS. THIS PAGE INDICATES SUCCESS. NAME: OGBUIGBA JENNIFER HOMA; MATRIC NUMBER: VUG/CSC/21/5201'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
